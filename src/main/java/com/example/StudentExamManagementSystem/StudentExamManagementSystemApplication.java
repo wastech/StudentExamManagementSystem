edc701685411fa7d910fc657eab4afb1ab4c37d8
wastech <fataiwasiu2@gmail.com>
2024-07-18 22:30:21 +0100

    set up the project :zap:

---
+++ src/main/java/com/example/StudentExamManagementSystem/StudentExamManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentExamManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExamManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentExamManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentExamManagementSystem/StudentExamManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentExamManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentExamManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
